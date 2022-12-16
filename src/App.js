e2f667552b2da1e5536b5571cfbf673e6846176e
gaoryrt <gaoryrt@gmail.com>
2022-12-16 14:16:52 +0800

    test: 💍 corr

--- src/App.js
+++ src/App.js
@@ -1,57 +1,115 @@
-import dayjs from 'dayjs';
-import React, { useState } from 'react';
-import './App.css';
+import dayjs from "dayjs";
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 export default function App() {
-  const [list, setList] = useState([{
-    key: 0,
-    name: 'a',
-    duration: 5,
-    lasttime: 1669893940000
-  }, {
-    key: 2,
-    name: '洗衣服（例）',
-    duration: 10,
-    lasttime: 1610184327000
-  }])
-  return <div className="list">{
-    list
-      .sort((b, a) => (b.lasttime + b.duration * 86400000) - (a.lasttime + a.duration * 86400000))
-      .map(o => <Card onRenew={() => {
-        setList(l => l.map(o2 => {
-          if (o2.key !== o.key) return o2
-          return {...o2, lasttime: +new Date()}
-        }))
-      }} onDel={() => {}} key={o.key} {...o}/>)
-  }
-  <div className='plus center' onClick={async () => {
-    const name = window.prompt('该活动叫什么')
-    const duration = window.prompt('每几天做一次')
-    const lasttime = dayjs(window.prompt('上次啥时候做的？YYYYMMDD')).valueOf()
-    setList(l => l.concat({
-      key: +new Date(),
-      name,
-      duration,
-      lasttime
-    }))
-  }}>
-    <svg width="40" height="40" viewBox="0 0 24 24"><path fill="currentColor" d="M19 12.998h-6v6h-2v-6H5v-2h6v-6h2v6h6z"></path></svg>  </div>
-  </div>
+  const [list, setList] = useState(JSON.parse(localStorage.getItem('list') || '[]'));
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
+  return (
+    <div className="list">
+      {list
+        .sort(
+          (b, a) =>
+            b.lasttime +
+            b.duration * 86400000 -
+            (a.lasttime + a.duration * 86400000)
+        )
+        .map((o) => (
+          <Card
+            onChange={(o3) => {
+              const newList = list.map((o2) => {
+                if (o2.key !== o.key) return o2;
+                return { ...o2, ...o3 };
+              });
+              setList(newList);
+            }}
+            onDel={() => {
+              if (window.confirm("确定删除本条？")) {
+                setList((l) => l.filter((o2) => o2.key !== o.key));
+              }
+            }}
+            key={o.key}
+            {...o}
+          />
+        ))}
+      <div
+        className="plus center"
+        onClick={async () => {
+          const name = window.prompt("活动叫什么");
+          if (!name) return;
+          const duration = window.prompt("每几天做一次");
+          const lasttime = dayjs(
+            window.prompt("上次啥时候做的？YYYYMMDD")
+          ).valueOf();
+          const newList = list.concat({
+            key: +new Date(),
+            name,
+            duration,
+            lasttime,
+          });
+          setList(newList);
+        }}
+      >
+        <svg width="40" height="40" viewBox="0 0 24 24">
+          <path
+            fill="currentColor"
+            d="M19 12.998h-6v6h-2v-6H5v-2h6v-6h2v6h6z"
+          ></path>
+        </svg>
+      </div>
+    </div>
+  );
 }
 
-
-const Card = ({name, duration, lasttime, onRenew, onDel}) => {
-  const diff = Math.floor((lasttime + duration * 86400000 - new Date()) / 86400000)
-  return <div className='line card' onDoubleClick={onDel}>
-    <div className="c">{name}</div>
-    <div className="grow"></div>
-    <div className='c'>
-      <div className="s">每 {duration} 天一次</div>
-      <div className="s g">上次：{dayjs(lasttime).format('YYYY-MM-DD')}</div>
+const Card = ({ name, duration, lasttime, onChange, onDel }) => {
+  const diff = Math.floor(
+    (lasttime + duration * 86400000 - new Date()) / 86400000
+  );
+  return (
+    <div className="line card">
+      <div
+        className="c"
+        onClick={() => {
+          const name = window.prompt("活动叫什么");
+          if (name) onChange({ name });
+        }}
+      >
+        {name}
+      </div>
+      <div className="grow" onClick={onDel}></div>
+      <div className="c">
+        <div
+          className="s"
+          onClick={() => {
+            const duration = window.prompt("每几天做一次");
+            if (duration) onChange({ duration });
+          }}
+        >
+          每 {duration} 天
+        </div>
+        <div
+          className="s g"
+          onClick={() => {
+            const lasttime = dayjs(
+              window.prompt("上次啥时候做的？YYYYMMDD")
+            ).valueOf();
+            onChange({ lasttime });
+          }}
+        >
+          {dayjs(lasttime).format("YYYY-MM-DD")}
+        </div>
+      </div>
+      <div
+        className="renew center"
+        onClick={() => {
+          if (window.confirm("已经完成了吗"))
+            onChange({ lasttime: +new Date() });
+        }}
+      >
+        <div className="b">{Math.max(0, diff)}</div>
+      </div>
     </div>
-    <div className="renew center" onClick={onRenew}>
-      <div className="b">{Math.max(0, diff)}</div>
-    </div>
-  </div>
-}
-
+  );
+};
